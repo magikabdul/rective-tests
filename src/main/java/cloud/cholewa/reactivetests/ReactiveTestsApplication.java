0ff2ef3d86d24ed7385d0ff234965fe612b31cb5
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-06-30 09:18:20 +0200

    init

---
+++ src/main/java/cloud/cholewa/reactivetests/ReactiveTestsApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.reactivetests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/reactivetests/ReactiveTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.reactivetests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
